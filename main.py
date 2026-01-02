d65dbbfd61f8c85b320eea6ef1dfce7e0dd21410
Ricardo Andrés Marino Rojas <47573394+Marinovsky@users.noreply.github.com>
2026-01-02 17:17:30 -0500

    Improve and add methods to reduce duplication

--- main.py
+++ main.py
@@ -154,6 +154,14 @@ class market_hours_database:
             print(f"Date {date} added it to {key} {label}")
             self.mhdb["entries"][key][label][date] = parsed_hour
             self.mhdb["entries"][key][label] = dict(sorted(self.mhdb["entries"][key][label].items(), key=lambda d: datetime.strptime(d[0], '%m/%d/%Y')))
+
+    def remove_date_to_dict(self, key, label, date_to_remove):
+        date = date_to_remove.strftime("%#m/%#d/%Y")
+        if label not in self.mhdb["entries"][key].keys():
+            print(f"{label} not present in {key} entry")
+        if date in self.mhdb["entries"][key][label].keys():
+            print(f"Date {date} removed from {key} {label}")
+            self.mhdb["entries"][key][label].pop(date, None)
     
     def add_date_to_list(self, key, label, date_to_add):
         date = date_to_add.strftime("%#m/%#d/%Y")
@@ -163,6 +171,14 @@ class market_hours_database:
             print(f"Date {date} added it to {key} {label}")
             self.mhdb["entries"][key][label].append(date)
             self.mhdb["entries"][key][label] = sorted(self.mhdb["entries"][key][label], key=lambda d: datetime.strptime(d, '%m/%d/%Y'))
+
+    def remove_date_from_list(self, key, label, date_to_remove):
+        date = date_to_remove.strftime("%#m/%#d/%Y")
+        if (key in self.mhdb["entries"].keys()) and (label not in self.mhdb["entries"][key].keys()):
+            print(f"{label} not present in {key} entry")
+        if (key in self.mhdb["entries"].keys()) and (date in self.mhdb["entries"][key][label]):
+            print(f"Date {date} removed from {key} {label}")
+            self.mhdb["entries"][key][label] = [e for e in self.mhdb["entries"][key][label] if e != date]
 
     def add_cme_late_open_to_mhdb(self, cme_class, late_open_date):
         entry = self.cme_group_futures_info[cme_class]
@@ -230,6 +246,7 @@ class market_hours_database:
             date = late_open_date.strftime("%#m/%#d/%Y")
             if "lateOpens" not in self.mhdb["entries"][key].keys():
                 continue
+            print(f"Date {date} removed from {key} late opens")
             self.mhdb["entries"][key]["lateOpens"].pop(date, None)
     
     def remove_holiday_from_mhdb(self, cme_class, holiday_date):
@@ -295,6 +312,10 @@ class market_hours_database:
     def add_holidays(self, key, holidays):
         for holiday in holidays:
             self.add_date_to_list(key, "holidays", holiday)
+    
+    def remove_holidays(self, key, holidays):
+        for holiday in holidays:
+            self.remove_date_from_list(key, "holidays", holiday)
 
     def add_cme_late_opens(self, cme_class, late_opens):
         for late_open in late_opens:
@@ -322,27 +343,27 @@ class market_hours_database:
     def apply_ice_changes(self, cme_class, changes):
         self.add_ice_holidays(cme_class, changes["ice"][cme_class]["holidays"])
     
-    def remove_early_closes(self, cme_class, early_closes):
+    def remove_cme_early_closes(self, cme_class, early_closes):
         for early_close in early_closes:
             self.remove_early_close_from_mhdb(cme_class, early_close)
 
-    def remove_late_opens(self, cme_class, late_opens):
+    def remove_cme_late_opens(self, cme_class, late_opens):
         for late_open in late_opens:
             self.remove_late_open_from_mhdb(cme_class, late_open)
     
-    def remove_holidays(self, cme_class, holidays):
+    def remove_cme_holidays(self, cme_class, holidays):
         for holiday in holidays:
             self.remove_holiday_from_mhdb(cme_class, holiday)
 
-    def remove_bank_holidays(self, cme_class, bank_holidays):
+    def remove_cme_bank_holidays(self, cme_class, bank_holidays):
         for bank_holiday in bank_holidays:
             self.remove_bank_holiday_from_mhdb(cme_class, bank_holiday)
 
     def remove_all(self, cme_class, changes):
-        self.remove_early_closes(cme_class, changes["cme"][cme_class]["remove"]["earlyCloses"])
-        self.remove_late_opens(cme_class, changes["cme"][cme_class]["remove"]["lateOpens"])
-        self.remove_holidays(cme_class, changes["cme"][cme_class]["remove"]["holidays"])
-        self.remove_bank_holidays(cme_class, changes["cme"][cme_class]["remove"]["bankHolidays"])
+        self.remove_cme_early_closes(cme_class, changes["cme"][cme_class]["remove"]["earlyCloses"])
+        self.remove_cme_late_opens(cme_class, changes["cme"][cme_class]["remove"]["lateOpens"])
+        self.remove_cme_holidays(cme_class, changes["cme"][cme_class]["remove"]["holidays"])
+        self.remove_cme_bank_holidays(cme_class, changes["cme"][cme_class]["remove"]["bankHolidays"])
 
     def parse_dictionary_of_dates(self, timezone, dates):
         dates_parsed = []
@@ -391,6 +412,13 @@ class market_hours_database:
         changes_df["cfe"] = {}
         changes_df["cfe"]["holidays"] = [datetime.strptime(f"{date}", "%m/%d/%Y") for date in changes["cfe"]["holidays"]]
         changes_df["cfe"]["earlyCloses"] = self.parse_dictionary_of_dates(timezone, changes["cfe"]["earlyCloses"])
+
+        changes_df["oanda"] = {}
+        changes_df["oanda"]["holidays"] = [datetime.strptime(f"{date}", "%m/%d/%Y") for date in changes["oanda"]["holidays"]]
+        changes_df["oanda"]["lateOpens"] = [datetime.strptime(f"{date}", "%m/%d/%Y") for date in changes["oanda"]["lateOpens"]]
+
+        changes_df["oanda"]["remove"] = {}
+        changes_df["oanda"]["remove"]["holidays"] = [datetime.strptime(f"{date}", "%m/%d/%Y") for date in changes["oanda"]["remove"]["holidays"]]
         
         return changes_df
 
@@ -406,44 +434,58 @@ class market_hours_database:
             outfile.write(json_object)
     
     def check_duplicates(self, cme_class):
-        print(f"Checking duplicates in {cme_class} entries...")
-        entry = self.cme_group_futures_info[cme_class]
-        products = entry["cmeKeys"]
-        for product in products.keys():
-            key = self.get_mhdb_key(product, products[product])
-            if key not in self.mhdb["entries"].keys(): continue
-
+        print(f"Checking duplicates in mhdb")
+        for key in self.mhdb["entries"].keys():
             holidays = [date for date in self.mhdb["entries"][key]["holidays"]]
             duplicates = {
                 date: count
                 for date, count in Counter(holidays).items()
                 if count > 1
             }
-            if len(duplicates) != 0: print(f"Duplicated {key} holidays: {duplicates}\n")
+            if len(duplicates) != 0:
+                print(f"Duplicated {key} holidays: {duplicates}\n")
+                for date in duplicates:
+                    date_formatted = datetime.strptime(f"{date}", "%m/%d/%Y")
+                    self.remove_date_from_list(key, "holidays", date_formatted)
 
-            bank_holidays = [date for date in self.mhdb["entries"][key]["bankHolidays"]]
-            duplicates = {
-                date: count
-                for date, count in Counter(bank_holidays).items()
-                if count > 1
-            }
-            if len(duplicates) != 0: print(f"Duplicated {key} bank holidays: {duplicates}\n")
-
-            early_closes = [date for date in self.mhdb["entries"][key]["earlyCloses"]]
-            duplicates = {
-                date: count
-                for date, count in Counter(early_closes).items()
-                if count > 1
-            }
-            if len(duplicates) != 0: print(f"Duplicated {key} early closes: {duplicates}\n")
-
-            late_opens = [date for date in self.mhdb["entries"][key]["lateOpens"]]
-            duplicates = {
-                date: count
-                for date, count in Counter(late_opens).items()
-                if count > 1
-            }
-            if len(duplicates) != 0: print(f"Duplicated {key} late opens: {duplicates}\n")
+            if "bankHolidays" in self.mhdb["entries"][key].keys():
+                bank_holidays = [date for date in self.mhdb["entries"][key]["bankHolidays"]]
+                duplicates = {
+                    date: count
+                    for date, count in Counter(bank_holidays).items()
+                    if count > 1
+                }
+                if len(duplicates) != 0:
+                    print(f"Duplicated {key} bank holidays: {duplicates}\n")
+                    for date in duplicates:
+                        date_formatted = datetime.strptime(f"{date}", "%m/%d/%Y")
+                        self.remove_date_from_list(key, "bankHolidays", date_formatted)
+            
+            if "earlyCloses" in self.mhdb["entries"][key].keys():
+                early_closes = [date for date in self.mhdb["entries"][key]["earlyCloses"]]
+                duplicates = {
+                    date: count
+                    for date, count in Counter(early_closes).items()
+                    if count > 1
+                }
+                if len(duplicates) != 0:
+                    print(f"Duplicated {key} early closes: {duplicates}\n")
+                    for date in duplicates:
+                        date_formatted = datetime.strptime(f"{date}", "%m/%d/%Y")
+                        self.remove_date_from_dict(key, "earlyCloses", date_formatted)
+            
+            if "lateOpens" in self.mhdb["entries"][key].keys():
+                late_opens = [date for date in self.mhdb["entries"][key]["lateOpens"]]
+                duplicates = {
+                    date: count
+                    for date, count in Counter(late_opens).items()
+                    if count > 1
+                }
+                if len(duplicates) != 0:
+                    print(f"Duplicated {key} late opens: {duplicates}\n")
+                    for date in duplicates:
+                        date_formatted = datetime.strptime(f"{date}", "%m/%d/%Y")
+                        self.remove_date_from_dict(key, "lateOpens", date_formatted)
     
     def find_new_entries(self):
         for entry in self.mhdb["entries"]:
@@ -460,7 +502,7 @@ class market_hours_database:
             if is_old_entry:
                 print(f"Product {cme_code} is a new entry")
     
-    def check_intersection_of_holidays(self, entry, label):
+    def check_intersection_of_holidays_and_label(self, entry, label):
         holidays = [date for date in self.mhdb["entries"][entry]["holidays"]]
         if label not in self.mhdb["entries"][entry].keys(): return
 
@@ -474,10 +516,56 @@ class market_hours_database:
 
     def check_disjoint_holidays(self):
         for entry in self.mhdb["entries"]:
-            self.check_intersection_of_holidays(entry, "earlyCloses")
-            self.check_intersection_of_holidays(entry, "lateOpens")
-            self.check_intersection_of_holidays(entry, "bankHolidays")
-            
+            self.check_intersection_of_holidays_and_label(entry, "earlyCloses")
+            self.check_intersection_of_holidays_and_label(entry, "lateOpens")
+            self.check_intersection_of_holidays_and_label(entry, "bankHolidays")
+    
+    def check_disjoint_holidays_with_parent(self):
+        for entry in self.mhdb["entries"]:
+            product = entry.split("-")
+            parent = f"{product[0]}-{product[1]}-[*]"
+            if (parent != entry) and (parent in self.mhdb["entries"].keys()):
+                product_holidays = [date for date in self.mhdb["entries"][entry]["holidays"]]
+                parent_holidays = [date for date in self.mhdb["entries"][parent]["holidays"]]
+
+                intersection = list(set(product_holidays) & set(parent_holidays))
+                if len(intersection) != 0:
+                    print(f"The following dates belong to both holidays of {entry} and its genric entry {parent}: {intersection}")
+                    print(f"Dates removed from {entry} holidays")
+                    self.mhdb["entries"][entry]["holidays"] = [date for date in self.mhdb["entries"][entry]["holidays"] if date not in intersection]
+
+                if "earlyCloses" in self.mhdb["entries"][entry].keys() and  "earlyCloses" in self.mhdb["entries"][parent].keys():
+                    product_early_closes = [date for date in self.mhdb["entries"][entry]["earlyCloses"].keys()]
+                    parent_early_closes = [date for date in self.mhdb["entries"][parent]["earlyCloses"].keys()]
+
+                    intersection = list(set(product_early_closes) & set(parent_early_closes))
+                    if len(intersection) != 0:
+                        print(f"The following dates belong to both early closes of {entry} and its genric entry {parent}: {intersection}")
+                        print(f"Dates removed from {entry} early Closes")
+                        for date in intersection:
+                            self.mhdb["entries"][entry]["earlyCloses"].pop(date, None)
+                
+                if "lateOpens" in self.mhdb["entries"][entry].keys() and  "lateOpens" in self.mhdb["entries"][parent].keys():
+                    product_late_opens = [date for date in self.mhdb["entries"][entry]["lateOpens"].keys()]
+                    parent_late_opens = [date for date in self.mhdb["entries"][parent]["lateOpens"].keys()]
+
+                    intersection = list(set(product_late_opens) & set(parent_late_opens))
+                    if len(intersection) != 0:
+                        print(f"The following dates belong to both late opens of {entry} and its genric entry {parent}: {intersection}")
+                        print(f"Dates removed from {entry} late opens")
+                        for date in intersection:
+                            self.mhdb["entries"][entry]["lateOpens"].pop(date, None)
+                
+                if "bankHolidays" in self.mhdb["entries"][entry].keys() and  "bankHolidays" in self.mhdb["entries"][parent].keys():
+                    product_bank_holidays = [date for date in self.mhdb["entries"][entry]["bankHolidays"]]
+                    parent_bank_holidays = [date for date in self.mhdb["entries"][parent]["bankHolidays"]]
+
+                    intersection = list(set(product_bank_holidays) & set(parent_bank_holidays))
+                    if len(intersection) != 0:
+                        print(f"The following dates belong to both bank holidays of {entry} and its genric entry {parent}: {intersection}")
+                        print(f"Dates removed from {entry} bank holidays")
+                        self.mhdb["entries"][entry]["holidays"] = [date for date in self.mhdb["entries"][entry]["holidays"] if date not in intersection]
+                print("\n")
 
 mhdb = market_hours_database()
 """
@@ -500,13 +588,12 @@ print("NICE")
 #mhdb.save_cme_group_futures_info()
 
 changes = mhdb.read_changes_from_json("changes.json")
-
-mhdb.add_holidays("Future-eurex-[*]", changes["eurex"]["holidays"])
-#mhdb.apply_ice_changes("generic", changes) // Apply them manually to keep the order
-mhdb.apply_ice_changes("forex", changes)
-mhdb.apply_ice_changes("agriculture", changes)
-mhdb.apply_ice_changes("energy", changes)
-
-#mhdb.add_holidays("Future-cfe-[*]", changes["cfe"]["holidays"]) // Apply them manually to keep the order
-mhdb.add_early_closes("Future-cfe-[*]", changes["cfe"]["earlyCloses"])
+mhdb.check_disjoint_holidays_with_parent()
+mhdb.check_disjoint_holidays()
+mhdb.check_duplicates("")
+mhdb.remove_all("dairy", changes)
+mhdb.remove_all("livestock", changes)
+mhdb.remove_all("lumber", changes)
+mhdb.remove_holidays("Forex-oanda-[*]", changes["oanda"]["remove"]["holidays"])
+mhdb.remove_date_from_list("Future-cbot-KE", "holidays", datetime(2023, 9, 4))
 mhdb.save()
